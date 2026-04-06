8f7c18e21908d338712bb24bc3e39ededcb4452e
Thao Nguyen <npthao.18@gmail.com>
2026-04-06 18:31:36 +0700

    final version - xu ly so co nhieu chu so

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const operatorBtn = document.querySelectorAll(".operator");
 const operatorArr = Array.from(operatorBtn);
 const equalBtn = document.querySelector("#but-equal");
 const deleteBtn = document.querySelector("#but-delete");
-let firstNum = null;
-let secondNum = null;
+let firstNum = 0;
+let secondNum = 0;
 let operator = null;
 
 const add = function (a,b) {
@@ -19,48 +19,63 @@ const multiply = function (a,b) {
     return (a*b);
 }
 const divide = function (a,b) {
-    return (a/b);
+    return (rounda/b);
 }
+
 
 const getNums = function(event) {
     let targetId = event.target.textContent;
-    // display.textContent = targetId;
-    if (firstNum === null) {
-        firstNum = parseInt(targetId, 10)
-        display.textContent = firstNum
+    let targetIdNum = parseInt(targetId, 10);
+    if (operator === null) {
+        firstNum = firstNum*10 + targetIdNum;
+        display.textContent = firstNum;
     } else {
-        secondNum = parseInt(targetId, 10)
-        display.textContent = secondNum
+        secondNum = secondNum*10 + targetIdNum;
+        display.textContent = secondNum;
     }
 }
 
 const handleOperatorBtn = function(event) {
+    if (operator !== null) {
+        handleEqualBtn();
+    }
     operator = event.target.id;
-    display.textContent = event.target.textContent;
-}
+    // event.target.style.backgroundColor = "pink";
+    // display.textContent = event.target.textContent;
+    }
+    
 
 const handleDeleteBtn = function(event) {
-    firstNum = null;
-    secondNum = null;
+    firstNum = 0;
+    secondNum = 0;
     operator = null;
     display.textContent = "";
 }
 
 const handleEqualBtn = function (event) {
-    if ((firstNum !== null) && (secondNum !== null)) {
+    console.log("firstNum ", firstNum, "secondNum ", secondNum, "operator ", operator)
+    if ((firstNum !== 0) && (secondNum !== 0)) {
         if (operator === "add") {
-            display.textContent = add(firstNum, secondNum)
+            display.textContent = add(firstNum, secondNum);
+            firstNum = add(firstNum, secondNum);
         }
         if (operator === "subtract") {
-            display.textContent = subtract(firstNum, secondNum)
+            display.textContent = subtract(firstNum, secondNum);
+            firstNum = subtract(firstNum, secondNum);
         }
         if (operator === "multiply") {
-            display.textContent = multiply(firstNum, secondNum)
+            display.textContent = multiply(firstNum, secondNum);
+            firstNum = multiply(firstNum, secondNum);``
         }
         if (operator === "divide") {
-            display.textContent = divide(firstNum, secondNum)
+            if (secondNum === 0) {
+                display.textContent = "Error divide by 0 ";
+            }
+            display.textContent = divide(firstNum, secondNum);
+            firstNum = divide(firstNum, secondNum);
         }
     }
+    secondNum = 0;
 }
 
 for (let index=0 ; index < digitArr.length ; index++) {
